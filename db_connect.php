7b4f8776c7277569ea4085f55f3e7972210e0697
abioyeayo <abioyeayo@gmail.com>
2025-04-07 14:43:09 +0100

    Update database connection

--- db_connect.php
+++ db_connect.php
@@ -5,9 +5,9 @@ class DB_Connect {
     
     public function connect() {
          define("DB_HOST", "localhost");
-         define("DB_USER", "u335940528_iot_cloud_user");
+         define("DB_USER", "u959043365_iot_cloud_user");
          define("DB_PASSWORD", "1B5#p!dV*");
-         define("DB_DATABASE", "u335940528_iot_cloud_db");
+         define("DB_DATABASE", "u959043365_iot_cloud_db");
          define("DB_PORT", 3308);
          
          $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT)or die(mysqli_connect_error());
